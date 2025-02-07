c2dbef8c21ce5dabdd1b994616de70c455db8510
Alexander <alexanderlee389@gmail.com>
2025-02-06 18:16:04 -0800

    moved react build to static

--- movie_streamer/movie_streamer/urls.py
+++ movie_streamer/movie_streamer/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('intro.urls')),
+    re_path(r'^.*$', TemplateView.as_view(template_name="react/index.html")),  # Serve React frontend
 ]
